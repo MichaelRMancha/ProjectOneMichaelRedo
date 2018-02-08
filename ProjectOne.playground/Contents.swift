8646ca16097ebf30ffb10c8448c9ae02a5aef29b
Michael Mancha <mancham@hawaii.edu>
2018-02-07 20:23:42 -1000

    Quick Save
    Completed majority of steps

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Variable Coding Challenge #1
+var firstString: String
+firstString = "This is my first string and "
+let secondString: String
+secondString = "this is a constant that cannot be changed"
+var thirdString = firstString + secondString
+print(thirdString)
+
+//Variable Coding Challenege #2
+let celcius: Float
+let fahrenheit: Float
+
+
+
+//Boolean Variables Coding Challenge #1
+
+//If Statement Coding Challenge #1
+var shortString = "This will be the shorter string"
+var longerString = "This will definitely be the longer string"
+var message: String
+if shortString < longerString {
+    message = "longer string is longer"
+} else {
+    message = "this string shouldn't be longer"
+}
+print(message)
+
+
+
